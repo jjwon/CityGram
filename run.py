e975107408232d74b644080320cb8d205e082789
Jason Won <jason.j.won@gmail.com>
2013-10-12 05:39:33 -0700

    First commit

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 import os
 from app import app
-port = int(os.environ.get("PORT", 5000))
-app.run(debug = True, port=port, host='0.0.0.0')
+
+if __name__ == "__main__":
+	port = int(os.environ.get("PORT", 5000))
+	app.run(debug = True, port=port, host='0.0.0.0')
